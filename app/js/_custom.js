2882bdb4b79829bc5be888ff249b96d61a0ed518
karapv <andrewkarapenkov@gmail.com>
2020-02-13 20:25:30 +0200

    Add structure

---
+++ app/js/_custom.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //Lazy Load
+    let lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazy"
+    });
+    lazyLoadInstance.update();
+
+});
+
